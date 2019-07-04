a975042192a5b4e687f6bfcaa23e7bd8adb8b2be
Timothy Hyatt <timothy.j.hyatt@gmail.com>
2019-07-04 13:48:14 +0930

    restyled restaurant display

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Display extends Component {
 
   render() { 
-    const {name, featured_image, cuisines, phone_numbers, timings, location} = this.props.restaurant.restaurant
+    const {name, featured_image, cuisines, phone_numbers, timings, location, price_range, user_rating} = this.props.restaurant.restaurant
 
     let openTimes = timings.split(',')
     
@@ -14,16 +14,15 @@ class Display extends Component {
           className="feature-image" 
           style={{ backgroundImage: `url(${featured_image})` }}
         >
-          <div className="name">{name}</div>
+          <div className="name">{name} <div className="small">{user_rating.aggregate_rating}</div></div>
           <div className="address">{location.address}</div>
           <div className="details">
-            <div><strong>Cuisines</strong><br/>{cuisines}</div>
-            <div><strong>Phone</strong><br/>{phone_numbers}</div>
-            <div><strong>Open</strong><br/> {openTimes.map((time, i) => { return <div key={i}>{time}</div> })}</div>
+            <div className="item"><strong>Cuisines</strong><br/>{cuisines}</div>
+            <div className="item"><strong>Price</strong><br/>{'$'.repeat(price_range)}</div>
+            <div className="item"><strong>Phone</strong><br/>{phone_numbers}</div>
+            <div className="item"><strong>Open times</strong><br/> {openTimes.map((time, i) => { return <div key={i}>{time}</div> })}</div>
           </div>
         </div>
-        <p>Rating: {this.props.restaurant.restaurant.user_rating.aggregate_rating}</p>
-        <p>Cost: {this.props.restaurant.restaurant.price_range}</p>
         
         
         
